d54eac46e7b1aa7890408cb4cd1eae0fb2d632b9
Dayeon Lee <iamdayeonlee@gmail.com>
2024-08-31 18:41:28 +0100

    Separate pages

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
 import "./App.css";
-import zoomImg from "./assets/zoom-img.png";
 import LinkedinPage from "./components/LinkedinPage";
-import ZoomHeader from "./components/ZoomHeader";
 import ZoomPage from "./components/ZoomPage";
 
 function App() {
   return (
     <>
+      <div className="flex justify-center items-center sticky top-0 h-20 bg-themeblue text-white font-semibold text-2xl z-50">
+        <span className="inline-block align-middle ">
+          Clone Coding: Login Form Page
+        </span>
+      </div>
       <div className="flex flex-col justfy-center items-center ">
-        <div className="flex flex-col h-[900px] w-max sm:w-screen justfy-center items-center">
-          <div className="text-2xl p-20 h-10">
-            <p>Clone coding: Linkedin Sign in form</p>
-          </div>
-          <LinkedinPage />
-        </div>
-        <div className="text-2xl p-20 h-10">
-          <p>Clone coding: Zoom Sign in form</p>
-        </div>
-        <div className="flex flex-col bg-white w-screen">
-          <ZoomHeader />
-          <div className="flex relative bg-white w-full sm:h-[900px] h-[1000px]">
-            <div className="bg-zoomgrey w-1/2 sm:p-[6rem]">
-              <div>
-                <div className="pt-[160px]">
-                  {/* desktop img */}
-                  <img src={zoomImg} alt="zoomimg" />
-                </div>
-              </div>
-            </div>
-            <div className="flex flex-col items-center bg-white pt-20 px-16 py-5 w-full sm:h-[900px] h-[1000px]">
-              <div className="w-[352px] h-[536px]">
-                <ZoomPage />
-              </div>
-            </div>
-          </div>
-        </div>
+        <LinkedinPage />
+        <ZoomPage />
       </div>
     </>
   );
--- src/components/ZoomPage.js
+++ src/components/ZoomPage.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 
+import ZoomHeader from "./ZoomHeader";
 import ZoomSignin from "./ZoomSignin";
 import ZoomSsoList from "./ZoomSsoList";
 import ZoomSupport from "./ZoomSupport";
 
-import zoomImg from "../assets/zoom-img.png";
 import zoomhelpbf from "../assets/zoom-help-1.png";
 import zoomhelpaf from "../assets/zoom-help-2.png";
+import zoomImg from "../assets/zoom-img.png";
 
 function ZoomPage() {
   const [isHelp, setIsHelp] = useState(false);
@@ -27,29 +28,46 @@ function ZoomPage() {
 
   return (
     <>
-      <div className="">
-        {/* mobile img */}
-        <div className="sm:hidden">
-          <img src={zoomImg} alt="zoomimg" />
+      <div className="flex flex-col bg-white w-screen">
+        <ZoomHeader />
+        <div className="flex relative bg-white w-full sm:h-[900px] h-[1000px]">
+          <div className="bg-zoomgrey w-1/2 sm:p-[6rem]">
+            <div>
+              <div className="pt-[160px]">
+                {/* desktop img */}
+                <img src={zoomImg} alt="zoomimg" />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col items-center bg-white pt-20 px-16 py-5 w-full sm:h-[900px] h-[1000px]">
+            <div className="w-[352px] h-[536px]">
+              <div className="">
+                {/* mobile img */}
+                <div className="sm:hidden">
+                  <img src={zoomImg} alt="zoomimg" />
+                </div>
+                <ZoomSignin help={isHelp} helpHandle={helpHandle} />
+                <ZoomSsoList />
+              </div>
+              <ZoomSupport close={isClose} closeHandle={handleClose} />
+              <button
+                className="absolute bottom-10 right-5"
+                onMouseEnter={handleHover}
+                onMouseLeave={handleHover}
+                onClick={handleClose}
+              >
+                {isHelp && (
+                  <img
+                    className="b-2"
+                    src={isHoverd ? zoomhelpaf : zoomhelpbf}
+                    alt="help"
+                  />
+                )}
+              </button>
+            </div>
+          </div>
         </div>
-        <ZoomSignin help={isHelp} helpHandle={helpHandle} />
-        <ZoomSsoList />
       </div>
-      <ZoomSupport close={isClose} closeHandle={handleClose} />
-      <button
-        className="absolute bottom-10 right-5"
-        onMouseEnter={handleHover}
-        onMouseLeave={handleHover}
-        onClick={handleClose}
-      >
-        {isHelp && (
-          <img
-            className="b-2"
-            src={isHoverd ? zoomhelpaf : zoomhelpbf}
-            alt="help"
-          />
-        )}
-      </button>
     </>
   );
 }
